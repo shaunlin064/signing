35b49a1c6837a60bd46841f624e2b2fe216d00e9
shaun <shaun@js-adways.com.tw>
2020-03-27 16:54:02 +0800

    常用表單 前端畫面切版 請款單 用印單 交際送禮

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,10 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import Vuex from 'vuex';
 
+
+Vue.use(Vuex);
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
