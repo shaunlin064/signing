d207280f375b683250b84a10c7a35fb2ccd0bd75
shaun <shaun@js-adways.com.tw>
2020-04-14 15:55:20 +0800

    form config update

--- config/form.php
+++ config/form.php
@@ -59,7 +59,7 @@
                 'form_stamp_type'     => [
                     'name' => '用印形式'
                 ],
-                'deploy'              => [
+                'deployed'            => [
                     'name' => '寄送方式'
                 ],
                 'recipient_company'   => [
@@ -96,28 +96,28 @@
                 'apply_subject'       => [
                     'name' => '項目'
                 ],
-                'customer_company'         => [
+                'customer_company'    => [
                     'name' => '客戶名稱'
                 ],
-                'customer_name'         => [
+                'customer_name'       => [
                     'name' => '客戶名稱'
                 ],
-                'attend_member'    => [
+                'attend_member'       => [
                     'name' => '出席人員'
                 ],
-                'type'             => [
+                'type'                => [
                     'name' => '類型'
                 ],
-                'date'             => [
+                'date'                => [
                     'name' => '事由日期'
                 ],
-                'cost'             => [
+                'cost'                => [
                     'name' => '預估費用'
                 ],
-                'remark'           => [
+                'remark'              => [
                     'name' => '備註'
                 ],
-                'apply_attachment' => [
+                'apply_attachment'    => [
                     'name' => '附件'
                 ],
             ]
@@ -137,27 +137,30 @@
                     'name' => '項目'
                 ],
                 'items'               => [
-                    'name' => '代墊項目',
+                    'name'       => '代墊項目',
                     'sub_column' => [
-                        'type' =>[
+                        'id'=>[
+                            'name' => 'id'
+                        ],
+                        'type'         => [
                             'name' => '類型'
                         ],
-                        'date' =>[
+                        'date'         => [
                             'name' => '日期'
                         ],
-                        'item' =>[
+                        'item'         => [
                             'name' => '項目'
                         ],
-                        'get_on_start' =>[
+                        'get_on_start' => [
                             'name' => '乘車起始點'
                         ],
-                        'campaign_id' =>[
+                        'campaign_id'  => [
                             'name' => '委刊編號'
                         ],
-                        'gift_id' =>[
+                        'gift_id'      => [
                             'name' => '交際送禮單號'
                         ],
-                        'price' =>[
+                        'price'        => [
                             'name' => '金額'
                         ],
                     ]
@@ -175,63 +178,66 @@
             'name'      => '差旅申請',
             'html_name' => 'form-travel_grant',
             'column'    => [
-                'apply_member_id'     => [
+                'apply_member_id'      => [
                     'name' => '申請人'
                 ],
-                'apply_department_id' => [
+                'apply_department_id'  => [
                     'name' => '請款單位'
                 ],
-                'apply_subject'       => [
+                'apply_subject'        => [
                     'name' => '項目'
                 ],
-                'accompany_user_id'     => [
+                'accompany_user_id'    => [
                     'name' => '出差人'
                 ],
-                'travel_date_start' =>[
+                'travel_date_start'    => [
                     'name' => '出差起始日期'
                 ],
-                'travel_date_end' =>[
+                'travel_date_end'      => [
                     'name' => '出差結束日期'
                 ],
-                'travel_location' =>[
+                'travel_location'      => [
                     'name' => '出差地點'
                 ],
                 'travel_stay_location' => [
                     'name' => '住宿地點'
                 ],
-                'predict_cost' => [
+                'predict_cost'         => [
                     'name' => '預估費用'
                 ],
-                'travel_remark' => [
-                  'name' => '出差事由詳述'
+                'travel_remark'        => [
+                    'name' => '出差事由詳述'
                 ],
-                'items'=>[
-                    'name' => '出差計畫',
+                'items'                => [
+                    'name'       => '出差計畫',
                     'sub_column' => [
-                        'date' =>[
+                        'id'=>[
+                            'name' => 'id'
+                        ],
+                        'date'             => [
                             'name' => '日期'
                         ],
-                        'customer_name' =>[
+                        'customer_name'    => [
                             'name' => '對象姓名'
                         ],
-                        'customer_company' =>[
+                        'customer_company' => [
                             'name' => '洽訪公司'
                         ],
-                        'meet_type' =>[
+                        'meet_type'        => [
                             'name' => '會議形式'
                         ],
-                        'agenda' =>[
+                        'agenda'           => [
                             'name' => '洽談內容'
                         ],
-                        'charge_user' =>[
+                        'charge_user'      => [
                             'name' => '負責業務'
                         ],
                     ]
                 ],
-                'remark'              => [
+                'remark'               => [
                     'name' => '備註'
                 ],
-                'apply_attachment'    => [
+                'apply_attachment'     => [
                     'name' => '附件'
                 ],
             ]
@@ -241,36 +247,51 @@
             'name'      => '差旅費用核銷',
             'html_name' => 'form-travel_fee',
             'column'    => [
-                'apply_member_id'     => [
+                'apply_member_id'      => [
                     'name' => '申請人'
                 ],
-                'apply_department_id' => [
+                'apply_department_id'  => [
                     'name' => '請款單位'
                 ],
-                'apply_subject'       => [
+                'apply_subject'        => [
                     'name' => '項目'
                 ],
-                'form_travel_grant_id'     => [
+                'form_travel_grant_id' => [
                     'name' => '差旅單'
                 ],
-                'items'=>[
-                    'name' => '出差計畫',
+                'items'                => [
+                    'name'       => '出差計畫',
                     'sub_column' => [
-                        'cost_type' =>[
-                            'name' => '費用類型'
+                        'id'=>[
+                           'name' => 'id'
                         ],
-                        'currency' =>[
-                            'name' => '幣別'
+                        'date'             => [
+                            'name' => '日期'
                         ],
-                        'fee' =>[
-                            'name' => '費用'
-                        ]
+                        'customer_name'    => [
+                            'name' => '對象姓名'
+                        ],
+                        'customer_company' => [
+                            'name' => '洽訪公司'
+                        ],
+                        'meet_type'        => [
+                            'name' => '會議形式'
+                        ],
+                        'agenda'           => [
+                            'name' => '洽談內容'
+                        ],
+                        'charge_user'      => [
+                            'name' => '負責業務'
+                        ],
+                        'fee_items'        => [
+                            'name' => '費用明細',
+                        ],
                     ]
                 ],
-                'remark'              => [
+                'remark'               => [
                     'name' => '備註'
                 ],
-                'apply_attachment'    => [
+                'apply_attachment'     => [
                     'name' => '附件'
                 ],
             ]
