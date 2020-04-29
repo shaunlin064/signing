2183b5182cd7606166ccc119e819254ce2aaf028
shaun <shaun@js-adways.com.tw>
2020-04-29 14:46:19 +0800

    請款單 排除 campaing_id 必填 表單無法判斷 user 請款類型 帶進一步 user feedback

--- resources/js/components/form/form-new.vue
+++ resources/js/components/form/form-new.vue
@@ -10,8 +10,8 @@
                             請選擇簽核表單
                         </label>
                         <select class="custom-select select2 form-control" :id="dom_target" :name="dom_target">
-                            <option value >請選擇</option>
-                            <option value="form-payment" >請款</option>
+                            <option value>請選擇</option>
+                            <option value="form-payment">請款</option>
                             <option value="form-sign">用印</option>
                             <option value="form-social">交際送禮</option>
                             <option value="form-refund">代墊</option>
@@ -28,35 +28,38 @@
             <component v-bind:is="form_type" :dom_id='form_type' :form_action='"new"' :can_edit='true'/>
         </keep-alive>
         <div class='row border-top-light mt-2 justify-content-end' v-show='form_type'>
-            <button type="submit" class="btn btn-primary mr-1 mb-1 waves-effect waves-light text-right mt-2" @click='submit' :disabled='lodding'>
+            <button type="submit" class="btn btn-primary mr-1 mb-1 waves-effect waves-light text-right mt-2"
+                    @click='submit' :disabled='lodding'>
                 <span role="status" aria-hidden="true" class="spinner-grow spinner-grow-sm" v-show='lodding'></span>
-                送出</button>
+                送出
+            </button>
         </div>
     </form>
 </template>
 
 <script>
-    import {mapState, mapMutations, mapActions, mapGetters} from 'vuex';
+    import {mapState} from 'vuex';
+
     export default {
         name: "form_new",
         components: {
             // Payment, Sign
         },
         props: {
-            form_config:Object,
+            form_config: Object,
         },
         data() {
             return {
-                form_type : '',
+                form_type: '',
                 already_open: [],
                 dom_id: 'form-new',
                 dom_target: 'form_type',
-                lodding : false,
-                validate_message : {},
+                lodding: false,
+                validate_message: {},
             }
         },
         computed: {
-            ...mapState(['form_submit_data','login_user', 'member', 'department']),
+            ...mapState(['form_submit_data', 'login_user', 'member', 'department']),
 
         },
         beforeMount: function () {
@@ -74,27 +77,27 @@
                 vue.form_type = event.target.value;
                 /*get formConfig*/
                 let selectFormConfig = {};
-                Object.keys(formConfig).forEach(key=>{
-                    if(formConfig[key]['html_name'] === vue.form_type){
+                Object.keys(formConfig).forEach(key => {
+                    if (formConfig[key]['html_name'] === vue.form_type) {
                         selectFormConfig = formConfig[key];
                     }
                 });
                 /*set default*/
-                if(eval(`this.form_submit_data['${vue.form_type}']`) === undefined){
+                if (eval(`this.form_submit_data['${vue.form_type}']`) === undefined) {
                     eval(`this.form_submit_data['${vue.form_type}'] = {};`);
                     let column = selectFormConfig.column;
 
                     eval(`vue.form_submit_data['${vue.form_type}']['form_id'] = '${selectFormConfig['id']}';`);
-                    Object.keys(column).forEach(columnName=>{
+                    Object.keys(column).forEach(columnName => {
                         /*TODO::set fake default 之後待討論驗證與形態問題*/
                         eval(`vue.form_submit_data['${vue.form_type}']['${columnName}'] = '';`);
 
                         /*default Array*/
-                        if($.inArray(columnName,['form_stamp_type','accompany_user_id','attend_member','apply_attachment']) != -1){
+                        if ($.inArray(columnName, ['form_stamp_type', 'accompany_user_id', 'attend_member', 'apply_attachment']) != -1) {
                             eval(`vue.form_submit_data['${vue.form_type}']['${columnName}'] = [];`);
                         }
                         /*default Object*/
-                        if($.inArray(columnName,['items']) != -1){
+                        if ($.inArray(columnName, ['items']) != -1) {
                             eval(`vue.form_submit_data['${vue.form_type}']['${columnName}'] = {};`);
                         }
                     });
@@ -102,30 +105,33 @@
                     eval(`vue.form_submit_data['${vue.form_type}']['apply_department_id'] = '${vue.login_user.department_id}';`);
                 }
             },
-            initial(){
+            initial() {
                 $(".select2").select2({
                     dropdownAutoWidth: true,
                     width: '100%'
                 });
-                $('#'+this.dom_target).change((e)=>{
+                $('#' + this.dom_target).change((e) => {
                     this.currentComponent(e);
                 });
             },
-            validate(){
+            validate() {
                 let vue = this;
                 let data = this.getPostData();
                 this.lodding = true;
                 vue.validate_message = {};
-                let checkColumn = ['apply_attachment','remark'];
+                let checkColumn = ['apply_attachment', 'remark'];
                 switch (vue.form_type) {
+                    case 'form-payment':
+                        checkColumn.push('campaign_id');
+                        break;
                     case 'form-sign':
-                        checkColumn.push('recipient_company','recipient_contact','recipient_phone','recipient_address');
+                        checkColumn.push('recipient_company', 'recipient_contact', 'recipient_phone', 'recipient_address');
                         break;
                 }
-                Object.keys(data).forEach( key => {
-                    if( data[key].length == 0) {
-                        if($.inArray(key, checkColumn) === -1){
-                            console.log(checkColumn,$.inArray(key, checkColumn));
+                Object.keys(data).forEach(key => {
+                    if (data[key].length == 0) {
+                        if ($.inArray(key, checkColumn) === -1) {
+                            console.log(checkColumn, $.inArray(key, checkColumn));
                             vue.lodding = false;
                             vue.validate_message[key] = 'required';
                         }
@@ -137,7 +143,7 @@
                 return false;
 
             },
-            submit(){
+            submit() {
                 this.lodding = true;
                 let data = this.getPostData();
                 let vue = this;
@@ -145,7 +151,7 @@
                 /*post validate*/
                 let type = this.validate();
                 console.log(type);
-                if(type){
+                if (type) {
                     return false;
                 }
                 console.log('post success');
@@ -170,20 +176,20 @@
                 //     });
 
             },
-            getPostData(){
-              return   Object.assign({},eval(`this.form_submit_data['${this.form_type}']`));
+            getPostData() {
+                return Object.assign({}, eval(`this.form_submit_data['${this.form_type}']`));
             },
-            toJson(data){
+            toJson(data) {
                 /* other Array to Json*/
-                Object.keys(data).forEach(columnName=> {
-                    if ($.inArray(columnName, ['form_stamp_type', 'accompany_user_id','attend_member','apply_attachment']) != -1) {
+                Object.keys(data).forEach(columnName => {
+                    if ($.inArray(columnName, ['form_stamp_type', 'accompany_user_id', 'attend_member', 'apply_attachment']) != -1) {
                         data[columnName] = JSON.stringify(data[columnName]);
                     }
                 });
                 /*travel_fee*/
-                if(data['items'] !== undefined){
+                if (data['items'] !== undefined) {
                     let itemsData = data['items'];
-                    Object.keys(itemsData).forEach(key=> {
+                    Object.keys(itemsData).forEach(key => {
 
                         if (itemsData[key]['fee_items'] !== undefined) {
                             itemsData[key]['fee_items'] = JSON.stringify(itemsData[key]['fee_items']);
@@ -198,28 +204,28 @@
             let vue = this;
             vue.initial();
             /*file upload*/
-            if(vue.already_open.indexOf(vue.form_type) === -1){
+            if (vue.already_open.indexOf(vue.form_type) === -1) {
                 vue.already_open.push(vue.form_type);
-                $("#"+vue.form_type+" #file_upload").dropzone({
+                $("#" + vue.form_type + " #file_upload").dropzone({
                     addRemoveLinks: true,
                     thumbnailWidth: 300,
                     thumbnailHeight: 300,
                     url: "/api/system/upload",
-                    init: function() {
+                    init: function () {
                         let formPostAttachment = vue.form_submit_data[vue.form_type].apply_attachment;
-                        this.on("sending", function(file, xhr, formData) {
+                        this.on("sending", function (file, xhr, formData) {
                             formData.append("dir", vue.form_type);
                             vue.lodding = true;
                         });
-                        this.on("success", function(file, responseText) {
-                            if(responseText.status != 1 ){
+                        this.on("success", function (file, responseText) {
+                            if (responseText.status != 1) {
                                 alert(responseText.status_string + responseText.message);
                                 return false;
                             }
                             let anchorEl = document.createElement('a');
-                            anchorEl.setAttribute('href',responseText.data.url);
-                            anchorEl.setAttribute('target','_blank');
-                            anchorEl.setAttribute('class','cursor-pointer');
+                            anchorEl.setAttribute('href', responseText.data.url);
+                            anchorEl.setAttribute('target', '_blank');
+                            anchorEl.setAttribute('class', 'cursor-pointer');
 
                             anchorEl.innerHTML = "<br>Download";
                             file.previewTemplate.appendChild(anchorEl);
@@ -234,9 +240,9 @@
                             vue.lodding = false;
                         });
                         this.on("removedfile", function (file) {
-                            vue.form_submit_data[vue.form_type].apply_attachment.map((e,k)=>{
+                            vue.form_submit_data[vue.form_type].apply_attachment.map((e, k) => {
                                 if (e.name === file.name) {
-                                    vue.form_submit_data[vue.form_type].apply_attachment.splice(k,1);
+                                    vue.form_submit_data[vue.form_type].apply_attachment.splice(k, 1);
                                     console.log(vue.form_submit_data[vue.form_type].apply_attachment);
                                 }
                             });
