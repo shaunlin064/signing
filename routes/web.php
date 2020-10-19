5d89f526ec3941cf5374ea5e43b9da2ae0572551
shaunlin064 <>
2020-10-19 15:48:12 +0800

    fix: 刪除測試路由

--- routes/web.php
+++ routes/web.php
@@ -2,57 +2,21 @@
 
 
     use App\Http\Controllers\API\FormController;
-
-    /*
-    |--------------------------------------------------------------------------
-    | Web Routes
-    |--------------------------------------------------------------------------
-    |
-    | Here is where you can register web routes for your application. These
-    | routes are loaded by the RouteServiceProvider within a group which
-    | contains the "web" middleware group. Now create something great!
-    |
-    */
-    Route::any(
-        '/getmodel', function(){
-        $obj = new \App\FormApplyCheckpoint();
-        $newData = $obj->find(1);
-        dd(\App\Signature::find(1)->formApplyCheckPoint->count() > 0 ? false : true);
-    });
-    Route::any(
-        '/testSignature', function(){
-            $obj = new \App\Http\Controllers\API\SignatureController();
-        $result = $obj->delete(newRequest([
-            'id' => 6 ]));
-        dd($result);
-    });
-    Route::any(
-        '/output', function(){
-        $signing = New \App\Http\Controllers\API\FormController();
-        $signing = $signing->get(
-            newRequest(
-                [
-                    'id'        => $_GET['id'],
-//                    'member_id' => 157
-                ]
-            )
-        )['data'];
-        $config = config('form')[$signing['form_id']];
-        if($signing['form_id'] === 6){
-            $signing['column']['items'] = collect($signing['column']['items'])->values();
-            $signing['fee_items_total'] = $signing['column']['items']->pluck('fee_items')->map(function($v,$k) use(&$signing){
-                $signing['column']['items'][$k]['fee_items'] = json_decode($v,true);
-                return json_decode($v,true);
-            })->flatten(1);
-        }
-        $html = $config['html_name'];
-        $pdf = PDF::loadView('pages.outPut.'.$html,['pdf' => true,'type'=>'payment','signing' => $signing,'config'=>$config]);
-        return $pdf->stream('test.pdf');
-
-    });
+	use App\Http\Controllers\API\SystemController;
+	use Illuminate\Http\Client\Request;
+	
+	/*
+	|--------------------------------------------------------------------------
+	| Web Routes
+	|--------------------------------------------------------------------------
+	|
+	| Here is where you can register web routes for your application. These
+	| routes are loaded by the RouteServiceProvider within a group which
+	| contains the "web" middleware group. Now create something great!
+	|
+	*/
     Route::post('form/print','PrintController@print');
     Route::post('pdf','PrintController@pdf');
-
     Route::view(
         '/', 'pages.customer.form-list', [
                'breadcrumbs' => [
@@ -64,102 +28,6 @@
                ]
            ]
     )->name('index');
-
-    Route::get('/test-formapply',function(){
-       $data = json_decode('{"form_id":"1","apply_member_id":"187","apply_department_id":"12","apply_subject":"1","campaign_id":null,"beneficiary":"1","receipt_number":"","receipt_date":"","receipt_price":"1","pay_type":"cash","transfer_date":"","remark":"","apply_attachment":"[]","created_at":"2020/5/28"}',true);
-        $obj  = new \App\Http\Controllers\API\FormController();
-
-        $obj->apply(newRequest($data));
-    });
-    Route::get(
-        '/test', function () {
-        //    api/form/depend
-        $obj  = new \App\Http\Controllers\API\FormFlowController();
-        $data =
-            json_decode(
-                '{"form_id":"1","form_flow_data":{"1":{"id":1,"name":"執行長室","form_id":1,"review_order":1,"review_type":1,"reviewer_id":106,"overwrite":0,"replace":true,"role":1,"replace_id":["157"],"component":"signatory"},"2":{"id":2,"name":"部級主管","form_id":1,"review_order":2,"review_type":2,"reviewer_id":1,"overwrite":1,"replace":0,"role":1,"replace_id":[],"component":"signatory"},"3":{"id":3,"name":"單位最高主管","form_id":1,"review_order":3,"review_type":2,"reviewer_id":3,"overwrite":1,"replace":0,"role":1,"replace_id":[],"component":"signatory"},"4":{"id":4,"name":"財務長","form_id":1,"review_order":4,"review_type":1,"reviewer_id":17,"overwrite":1,"replace":0,"role":1,"replace_id":[],"component":"signatory"},"5":{"id":5,"name":"執行長","form_id":1,"review_order":5,"review_type":1,"reviewer_id":15,"overwrite":0,"replace":0,"role":1,"replace_id":[],"component":"signatory"},"6":{"id":6,"name":"財務","form_id":1,"review_order":6,"review_type":1,"reviewer_id":202,"overwrite":0,"replace":1,"role":2,"replace_id":[63,178,203],"component":"signatory"}}}',true)
-        ;
-        dd($obj->saveAll(newRequest($data)));
-    }
-    );
-    Route::get(
-        '/check', function () {
-        //    api/form/depend
-        $api  = new FormController();
-        $data = newRequest(
-            [
-                'member_id' => 157,
-                'id'        => 96,
-                'status'    => 2
-            ]
-        );
-        $api->check($data);
-    }
-    );
-
-    Route::get(
-        '/depend', function () {
-        //    api/form/depend
-        $api  = new FormController();
-        $data = newRequest(
-            [
-                'member_id' => 157,
-                'form_id'   => 5
-            ]
-        );
-        $api->depend($data);
-    }
-    );
-    Route::get(
-        '/apply', function () {
-        $api  = new FormController();
-        $data = newRequest(
-            [
-                'member_id' => 157,
-                'form_id'   => 5
-            ]
-        );
-        dd($api->checkList($data));
-    });
-
-    Route::group([
-                     'middleware' => ['apiCheck']
-                 ], function() {
-//        Route::get(
-//            '/apply', function () {
-//            dd(
-//                session()->all(), Cache::store('memcached')->get('login_user_log'), $_SERVER['REMOTE_ADDR'],
-//                $localIP = getHostByName(getHostName())
-//            );
-//
-//
-//            $data = [
-//                'apply_attachment'    => "[]",
-//                'apply_department_id' => "41",
-//                'apply_member_id'     => "157",
-//                'apply_subject'       => "test",
-//                'form_id'             => "6",
-//                'form_pair_data_id'   => "1",
-//                'items'               => [
-//                    'id'               => "0",
-//                    'meet_type'        => "1",
-//                    'agenda'           => "1",
-//                    'charge_user'      => "157",
-//                    'customer_company' => "1",
-//                    'customer_name'    => "1",
-//                    'date'             => "2020-05-07",
-//                    'fee_items'        => '{"0":{"id":"0","type":"交通","currency":"TWD","fee":"3"}}',
-//                ],
-//            ];
-//            $api  = new FormController();
-//            $data = newRequest(
-//                $data
-//            );
-//            $api->apply($data);
-//
-//        }
-//        );
-    });
     // Route Authentication Pages
     Route::get('/login', 'AuthenticationController@view')->name('login');
     Route::post('/remoteLogin', 'AuthenticationController@login')->name('remoteLogin');
